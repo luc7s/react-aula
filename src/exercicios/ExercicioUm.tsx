87e2b46e7a7bc9e91c33dd2e6ef302ec39d2e601
Luc7s <lucasferrokuka@gmail.com>
2026-04-24 09:08:33 -0300

    primeiro ex

--- src/exercicios/ExercicioUm.tsx
+++ src/exercicios/ExercicioUm.tsx
@@ -1,12 +1,55 @@
 ﻿import ExercicioPlaceholder from "../components/ExercicioPlaceholder";
-
+import { useState } from "react";
+const MAXIMO = 10;
+const MINIMO = 0;
 function ExercicioUm() {
-  return (
-    <ExercicioPlaceholder
-      titulo="Exercicio 01 - Contador com Limites"
-      arquivoMarkdown="01-contador-com-limites.md"
-    />
-  );
+  //por toda a logica aqui dentro
+const [contador, setContador] = useState(0);
+function incrementar() {
+  if (contador < MAXIMO ) 
+  setContador(contador + 1);
 }
+const descontar = () => {
+  if (contador > MINIMO )
+  setContador(contador - 1);
+}
+const resetar = () => {
+  setContador(0);
+}
+let mensagemLimite = 'Está dentro do permitido!';
+if (contador === MINIMO) mensagemLimite = 'Está no minimo'
+if (contador === MAXIMO) mensagemLimite = 'Está no maximo'
+return (
+  <div className="flex flex-col items-center gap-4 p-10">
+    <h1 className="text-4xl font-bold">{contador}</h1>
+
+    <div className="flex gap-2">
+      <button
+        className="px-4 py-2 bg-green-500 text-white rounded"
+        onClick={incrementar}
+      >
+        +1
+      </button>
+
+      <button
+        className="px-4 py-2 bg-red-500 text-white rounded"
+        onClick={descontar}
+      >
+        -1
+      </button>
+      <button
+        className="px-4 py-2 bg-gray-500 text-white rounded"
+        onClick={resetar}
+      >
+        Reset
+      </button>
+
+    </div>
+    <p className="text-xl font-medium mt-2 text-gray-700">{mensagemLimite} </p>
+  </div>
+  
+);
+}
+
 
 export default ExercicioUm;
